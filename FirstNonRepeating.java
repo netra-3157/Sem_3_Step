19259f236e3cf79d3ade39c2f4c77567ca087270
netra-3157 <netravenkat2531@gmail.com>
2026-09-14 22:52:12 +0530

    Added NonRepeating

---
+++ FirstNonRepeating.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class FirstNonRepeating {
+
+    // Method to find first non-repeating character
+    static char findFirstNonRepeatingChar(String text) {
+
+        int[] frequency = new int[256];
+
+        // Count frequency of each character
+        for (int i = 0; i < text.length(); i++) {
+            frequency[text.charAt(i)]++;
+        }
+
+        // Find first character with frequency 1
+        for (int i = 0; i < text.length(); i++) {
+            if (frequency[text.charAt(i)] == 1) {
+                return text.charAt(i);
+            }
+        }
+
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word or sentence: ");
+        String text = sc.nextLine();
+
+        char result = findFirstNonRepeatingChar(text);
+
+        if (result != '\0') {
+            System.out.println("First Non-Repeating Character: '" + result + "'");
+        } else {
+            System.out.println("No Non-Repeating Character Found");
+        }
+
+        sc.close();
+    }
+}
